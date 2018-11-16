d231abf9b93ab46c7ffc634ea6503da0db5a87e2
liubinwyzbt <liumowang@aliyun.com>
2018-11-16 17:32:35 +0800

    增加微信商户给用户发放零钱

--- src/main/java/com/yszx/module/pay/wxpay/WxpayUtil.java
+++ src/main/java/com/yszx/module/pay/wxpay/WxpayUtil.java
@@ -10,6 +10,13 @@ import java.net.InetAddress;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -19,9 +26,28 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.UUID;
 import java.util.Map.Entry;
 
+import javax.net.ssl.SSLContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.httpclient.NameValuePair;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 
 import com.xiaodajia.framework.exception.BaseRuntimeWithCodeException;
 import com.xiaodajia.framework.exception.ErrorCodelEnum;
@@ -29,6 +55,7 @@ import com.xiaodajia.framework.util.string.StringUtils;
 import com.yszx.module.core.dto.WxParamDTO;
 import com.yszx.module.pay.config.WxPayConfig;
 import com.yszx.module.pay.util.MD5Util;
+import com.yszx.module.pay.util.RequestHandler;
 import com.yszx.module.pay.util.XMLUtil;
 import com.yszx.module.util.LogUtils;
 
@@ -407,6 +434,151 @@ public class WxpayUtil {
 			wxPayResult.put("codeMsg", "系统异常！");
 		}
 		return wxPayResult;
+	}
+	
+	/**
+	 * @Description：微信支付，企业向个人付款
+	 * @param openid 收款人的openID(微信的openID)
+	 * @param amount 付款金额
+	 * @param desc 付款描述
+	 * @param partner_trade_no 订单号(系统业务逻辑用到的订单号)
+	 * @return map{state:SUCCESS/FAIL}{payment_no:
+	 *         '支付成功后，微信返回的订单号'}{payment_time:'支付成功的时间'}{err_code:'支付失败后，返回的错误代码'}{err_code_des:'支付失败后，返回的错误描
+	 *         述 ' }
+	 * @throws ParseException
+	 * @throws IOException
+	 * @throws NoSuchAlgorithmException
+	 * @throws CertificateException
+	 * @throws KeyStoreException
+	 * @throws KeyManagementException
+	 * @throws UnrecoverableKeyException
+	 * @throws DocumentException
+	 */
+	public static Map<String, String> transferToUser(WxParamDTO wxParam, int amount, String desc, 
+			String partner_trade_no,HttpServletRequest requesty,HttpServletResponse responsey) throws Exception {
+		Map<String, String> map = new HashMap<String, String>(); // 定义一个返回MAP
+		if(StringUtils.isEmpty(wxParam.getOpenId())){
+			throw new Exception("用户OpenId为空");
+		}
+		try {
+			// 读取配置文件信息，包括微信支付的APPID，商户ID和证书路径
+//			InputStream configFile = WeixinDraw.class.getResourceAsStream("weixin.config.xml");
+//			SAXReader reader = new SAXReader();
+//			Document doc = reader.read(configFile);
+//			Element config = doc.getRootElement();
+
+			String url = "https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers";
+//			InetAddress ia = InetAddress.getLocalHost();
+			String ip = requesty.getRemoteAddr(); // 获取本机IP地址
+			String uuid = UUID.randomUUID().toString().toUpperCase().replaceAll("-", "");// 随机获取UUID
+//			String appid = config.elementTextTrim("appKey");// 微信分配的公众账号ID（企业号corpid即为此appId）
+//			String mchid = config.elementTextTrim("mchid");// 微信支付分配的商户号
+			// 设置支付参数
+			SortedMap<String, String> signParams = new TreeMap<String, String>();
+
+			signParams.put("mch_appid", wxParam.getAppId()); // 微信分配的公众账号ID（企业号corpid即为此appId）
+			signParams.put("mchid", wxParam.getMerchant());// 微信支付分配的商户号
+			signParams.put("nonce_str", uuid); // 随机字符串，不长于32位
+			signParams.put("partner_trade_no", partner_trade_no); // 商户订单号，需保持唯一性
+			signParams.put("openid", wxParam.getOpenId()); // 商户appid下，某用户的openid
+			signParams.put("check_name", "NO_CHECK"); // NO_CHECK：不校验真实姓名
+														// FORCE_CHECK：强校验真实姓名（未实名认证的用户会校验失败，无法转账）
+														// OPTION_CHECK：针对已实名认证的用户才校验真实姓名（未实名认证用户不校验，可以转账成功）
+			signParams.put("amount", amount+""); // 企业付款金额，单位为分
+			signParams.put("desc", desc); // 企业付款操作说明信息。必填。
+			signParams.put("spbill_create_ip", ip); // 调用接口的机器Ip地址
+
+			// 生成支付签名，要采用URLENCODER的原始值进行MD5算法！
+			
+			RequestHandler reqHandler = new RequestHandler(requesty, responsey);
+			reqHandler.init( wxParam.getAppId(),  wxParam.getAppSecret(),  wxParam.getMerchantkey());
+			String sign = reqHandler.createSign(signParams);
+			
+//			String sign = "";
+//			sign = createSign("UTF-8", signParams);
+			// System.out.println(sign);
+			String data = "<xml><mch_appid>";
+			data += wxParam.getAppId() + "</mch_appid><mchid>"; // APPID
+			data += wxParam.getMerchant() + "</mchid><nonce_str>"; // 商户ID
+			data += uuid + "</nonce_str><partner_trade_no>"; // 随机字符串
+			data += partner_trade_no + "</partner_trade_no><openid>"; // 订单号
+			data += wxParam.getOpenId() + "</openid><check_name>NO_CHECK</check_name><amount>"; // 是否强制实名验证
+			data += amount + "</amount><desc>"; // 企业付款金额，单位为分
+			data += desc + "</desc><spbill_create_ip>"; // 企业付款操作说明信息。必填。
+			data += ip + "</spbill_create_ip><sign>";// 调用接口的机器Ip地址
+			data += sign + "</sign></xml>";// 签名
+			System.out.println(data);
+			// 获取证书，发送POST请求；
+			KeyStore keyStore = KeyStore.getInstance("PKCS12");
+			InputStream instream = null;
+			try{
+				instream = WxpayUtil.class.getResourceAsStream("/apiclient_cert.p12");
+				//FileInputStream instream = new FileInputStream(new File("/opt/web/wjhl_h5/cert/apiclient_cert.p12")); // 从配置文件里读取证书的路径信息
+//				FileInputStream instream = new FileInputStream(new File(config.elementTextTrim("cert_path"))); // 从配置文件里读取证书的路径信息
+				keyStore.load(instream, wxParam.getMerchant().toCharArray());// 证书密码是商户ID
+			}catch(Exception e){
+				System.out.println("微信零钱发放=" + e.getMessage());
+			}finally{
+				if(null != instream){
+					instream.close();
+				}
+			}
+			SSLContext sslcontext = SSLContexts.custom().loadKeyMaterial(keyStore, wxParam.getMerchant().toCharArray()).build();
+			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, new String[] { "TLSv1" }, null, SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+			CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+			HttpPost httpost = new HttpPost(url); //
+			httpost.addHeader("Connection", "keep-alive");
+			httpost.addHeader("Accept", "*/*");
+			httpost.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+			httpost.addHeader("Host", "api.mch.weixin.qq.com");
+			httpost.addHeader("X-Requested-With", "XMLHttpRequest");
+			httpost.addHeader("Cache-Control", "max-age=0");
+			httpost.addHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0) ");
+			httpost.setEntity(new StringEntity(data, "UTF-8"));
+			CloseableHttpResponse response = httpclient.execute(httpost);
+			HttpEntity entity = response.getEntity();
+
+			String jsonStr = EntityUtils.toString(response.getEntity(), "UTF-8");
+			EntityUtils.consume(entity);
+			// 把返回的字符串解释成DOM节点
+			Document dom = DocumentHelper.parseText(jsonStr);
+			Element root = dom.getRootElement();
+			String returnCode = root.element("result_code").getText(); // 获取返回代码
+			if (StringUtils.equals(returnCode, "SUCCESS")) { // 判断返回码为成功还是失败
+				String payment_no = root.element("payment_no").getText(); // 获取支付流水号
+				String payment_time = root.element("payment_time").getText(); // 获取支付时间
+				map.put("state", returnCode);
+				map.put("payment_no", payment_no);
+				map.put("payment_time", payment_time);
+				return map;
+			} else {
+				String err_code = root.element("err_code").getText(); // 获取错误代码
+				String err_code_des = root.element("err_code_des").getText();// 获取错误描述
+				map.put("state", returnCode);// state
+				map.put("err_code", err_code);// err_code
+				map.put("err_code_des", err_code_des);// err_code_des
+				return map;
+			}
+
+		} catch (DocumentException ex) {
+			ex.printStackTrace();
+			return map;
+		} catch (UnrecoverableKeyException ex) {
+			ex.printStackTrace();
+			return map;
+		} catch (KeyManagementException ex) {
+			ex.printStackTrace();
+			return map;
+		} catch (KeyStoreException ex) {
+			ex.printStackTrace();
+			return map;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return map;
+		} catch (NoSuchAlgorithmException ex) {
+			ex.printStackTrace();
+			return map;
+		}
 	}
 
 	public static String createSign(String characterEncoding,
